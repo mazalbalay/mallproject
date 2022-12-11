7726c14bcedc7998a368ec95bd83de56bce17c14
haimarka <haimarka1@gmail.com>
2022-12-11 15:08:16 +0200

    oshrat update

--- server/ROUTERS/userRoute.js
+++ server/ROUTERS/userRoute.js
@@ -5,12 +5,16 @@ const BLL = require("../BLLS/usersProductBLL");
 
 const router = express.Router();
 
-router.post("/user", BLL.register());
+router.post("/singUp", BLL.singUp());
 
-router.put("/user/:id",BLL.updateUser() )
+router.post("/singIn", BLL.singIn());
+
+router.get("/user/:id", BLL.getUserById());
+
+router.put("/user/:id",BLL.updateUser())
 
 router.delete("/user/:id",BLL.deleteUser())
 
-router.get("/user", BLL.getAllUsers());
+router.get("/user", BLL.getAllUsers())
 
 module.exports = router;
--- server/main.js
+++ server/main.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors")
 const userRouter = require('./ROUTERS/userRoute')
-// const mongoose = require("mongoose");
 
 require("./config")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/users' , userRouter)
+app.use('/' , userRouter)
 app.listen(8000, ()=>console.log("app listen"))
 
