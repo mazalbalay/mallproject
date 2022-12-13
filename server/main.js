d4d88494604082b361bf49e2c286bb55dfd8df3b
Yosef <yosef.manyiav@gmail.com>
2022-12-13 11:50:34 +0200

    yosef

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const orderRouter = require("./ROUTERS/orderRouter")
 const userRouter = require('./ROUTERS/userRoute')
+const productRouter = require('./ROUTERS/productRouter')
 // const mongoose = require("mongoose");
 
 require("./config")
@@ -10,5 +11,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/' , userRouter)
+app.use('/' , productRouter)
 app.listen(8000, ()=>console.log("app listen"))
 
