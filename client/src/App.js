963879255f3783730f081271eec92325a084ba3e
mazal balay <mazalbalay3@gmail.com>
2022-12-20 17:33:09 +0200

    mazal

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,31 @@ import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import CheckOut from "./components/checkout/CheckOut";
 import CheckOutNav from "./components/CheckOutNav";
 import Auth from "./components/authentication/Auth";
-import ThancksPage from './components/checkout/ThancksPage'
+import ThancksPage from "./components/checkout/ThancksPage";
 import Home from "./components/Home";
 import ErrPage from "./components/checkout/ErrPage";
 import ChangePWD from "./components/userProfile/ChangePWD";
 import PersonalInfo from "./components/userProfile/PersonalInfo";
 import UserProfile from "./components/userProfile/UserProfile";
-import AdminMain from './components/Admin/Main';
-import CreateDepartment from './components/Admin/CreateDepartment';
-import CreateStore from './components/Admin/CreateStore';
+import AdminMain from "./components/Admin/Main";
+import CreateDepartment from "./components/Admin/CreateDepartment";
+import CreateStore from "./components/Admin/CreateStore";
 import Departments from "./components/Admin/Departments";
-import Stores from './components/Admin/Stores';
+import Stores from "./components/Admin/Stores";
 import EditDepartment from "./components/Admin/EditDepartment";
 import EditStore from "./components/Admin/EditStore";
-import MainPage from './components/MainPage'
+import MainPage from "./components/MainPage";
 import Department from "./components/Department";
 import Footer from "./components/Footer";
 
 function App() {
-
- 
   return (
-     <PayPalScriptProvider options={{"client-id":"ATZnLpZdreIw8GDvsCs-eguhcXT3gn4gmDrTi8L0n0arR08UvMJeSOoAeVQCJSFuaC-2EWi669UKRbUc"}}>
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+          "ATZnLpZdreIw8GDvsCs-eguhcXT3gn4gmDrTi8L0n0arR08UvMJeSOoAeVQCJSFuaC-2EWi669UKRbUc",
+      }}
+    >
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -34,12 +37,15 @@ function App() {
           <Route path="/changepassword" element={<ChangePWD />} />
           <Route path="/ThancksPage" element={<ThancksPage />} />
           <Route path="/ErrPage" element={<ErrPage />} />
-          <Route path="/userprofile" element={< UserProfile/>} />
-          <Route path="/personalinfo" element={< PersonalInfo/>} />  
-          <Route path="/Admin" element={<AdminMain/>} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/personalinfo" element={<PersonalInfo />} />
+          <Route path="/Admin" element={<AdminMain />} />
           <Route path="/Admin/department/new/" element={<CreateDepartment />} />
           <Route path="/Admin/store/new/" element={<CreateStore />} />
-          <Route path="/Admin/department/edit/;id" element={<EditDepartment />} />
+          <Route
+            path="/Admin/department/edit/;id"
+            element={<EditDepartment />}
+          />
           <Route path="/Admin/store/edit/;id" element={<EditStore />} />
           <Route path="/Admin/stores" element={<Stores />} />
           <Route path="/Admin/departments" element={<Departments />} />
@@ -47,7 +53,7 @@ function App() {
         </Routes>
       </div>
     </PayPalScriptProvider>
-    );
+  );
 }
 
 export default App;
