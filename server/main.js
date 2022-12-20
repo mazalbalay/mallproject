f2b0d24ae79668fe87ea989c3d2b149ca503953d
shlomit <shlomitshifraw@gmail.com>
2022-12-20 16:50:52 +0200

    update

--- server/main.js
+++ server/main.js
@@ -1,56 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-<<<<<<< HEAD
-const userRouter = require("./ROUTERS/userRoute");
-const productRouter = require("./ROUTERS/productRouter");
-const orderRouter = require("./ROUTERS/orderRouter");
-const nodemailer = require("nodemailer");
-const cors = require("cors");
-
-// const sendEmail = () => {
-//   return new Promise((resolve, reject) => {
-//     const transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: "",
-//         pass: "",
-//       },
-//     });
-//     const mailOptions = {
-//         from: '', // sender address
-//         to: '', // list of receivers
-//         subject: 'Subject of your email', // Subject line
-//         html: '<p>Your html here</p>'// plain text body
-//       };
-
-//       transporter.sendMail(mailOptions, function (err, info) {
-//         if(err)
-//           console.log(err)
-//         else
-//           console.log(info);
-//      });
-//   });
-// };
-
-require("./config");
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/", userRouter);
-app.use("/", orderRouter);
-app.use("/", productRouter);
-
-// app.get("/",(req,res)=>{
-//     sendEmail()
-//     .then(response=>res.send(response.message))
-//     .catch(err=>res.status(500).send(err.message))
-// })
-
-app.listen(8000, () => console.log("app listen"));
-=======
-const bodyParser = require("body-parser") 
+const bodyParser = require("body-parser")
 const cors = require("cors");
 // const nodemailer = require("nodemailer");
 const userRouter = require("./ROUTERS/userRoute");
@@ -95,11 +45,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }));
 app.use("/", userRouter);
 app.use("/", orderRouter);
 app.use("/", productRouter);
-app.get("/",(req,res)=>{
-    sendEmail()
-    .then(response=>res.send(response.message))
-    .catch(err=>res.status(500).send(err.message))
-})
+// app.get("/",(req,res)=>{
+//     sendEmail()
+//     .then(response=>res.send(response.message))
+//     .catch(err=>res.status(500).send(err.message))
+// })
 
 app.listen(8000, () => console.log("app listen"));
->>>>>>> origin
