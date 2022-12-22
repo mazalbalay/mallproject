213f6617626c6a5f0f4aa11a51de244c06847e98
Yosef <yosef.manyiav@gmail.com>
2022-12-22 14:15:48 +0200

    schema

--- client/src/components/Admin/CreateDepartment.jsx
+++ client/src/components/Admin/CreateDepartment.jsx
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from "react";
-import {createDepartment,getDepartments} from './ApiCalls/Departments'
+import {createDepartment,getDepartments} from '../ApiCalls/Departments'
 import FileBase64 from 'react-file-base64';
 
 
