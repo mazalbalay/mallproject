54b7176276ab9bf178158011833a067e532a1b23
shlomit <shlomitshifraw@gmail.com>
2022-12-25 19:54:38 +0200

    updating home.js

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import MainPage from './MainPage';
 import MainNav  from "./Navs/MainNav";
-import Footer from './Footers/MainPageFooter'
-import AboutPage from './AboutPage/AboutPage';
-import ProductsScreen from './Manager/ProductsScreen';
-import MainPageFooter from './Footers/MainPageFooter';
+import Footer from './Footers/Footer';
 export default function Home() {
   return (
     <div>
