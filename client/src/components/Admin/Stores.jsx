3c777ae0482239c06d064ba89500e52bbbe53721
aviva itzack <aviva8999@gmail.com>
2022-12-25 15:07:39 +0200

    update

--- client/src/components/Admin/Stores.jsx
+++ client/src/components/Admin/Stores.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { getStores } from "../ApiCalls/Store";
 import { useNavigate } from "react-router-dom";
 import StoreComp from "./StoreComp";
+import Nav from '../Navs/MainNav';
+import MainPageFooter from "../Footers/MainPageFooter";
 
 export default function Stores() {
   const [Stores, setStores] = useState([]);
@@ -17,10 +19,13 @@ export default function Stores() {
     navigate(`${route}`);
   }
   return (
+    <div>
+      <Nav/>
+    
     <div className="flex-col text-center w-full p-20">
       <div className="w-2/4 m-auto">
         <button
-          className=" bg-sky-300 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none  focus:shadow-outline"
+          className="w-full bg-sky-300 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none  focus:shadow-outline"
           type="button"
           onClick={() => navigateTo(`${"/admin/Store/new"}`)}
         >
@@ -39,6 +44,8 @@ export default function Stores() {
           ))}
         </div>
       </div>
+    </div>
+    <MainPageFooter/>
     </div>
   );
 }
